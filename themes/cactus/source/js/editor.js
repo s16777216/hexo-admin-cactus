db01a6d84d21145990ef4da67c5d00ac15b59c94
s16777216 <l1597532@gmail.com>
2025-09-06 23:59:43 +0800

    Refactor code structure for improved readability and maintainability

--- themes/cactus/source/js/editor.js
+++ themes/cactus/source/js/editor.js
@@ -1,37 +1,8 @@
-//#region Editor Component
-class Editor extends HTMLElement {
-    #textarea;
-    constructor() {
-        super();
-        this.innerHTML = /*html*/ `
-        <div>
-            <textarea></textarea>
-        </div>
-        `;
-
-        this.#textarea = this.querySelector('textarea');
-    }
-
-    handleInput(event) {
-        const value = event.target.value;
-        console.log('Input event:', value);
-    }
-
-    connectedCallback() {
-        this.#textarea.addEventListener('input', this.handleInput);
-    }
-
-    disconnectedCallback() {
-        this.#textarea.removeEventListener('input', this.handleInput);
-    }
-
-    get value() {
-        return this.#textarea.value;
-    }
-
-    set value(val) {
-        this.#textarea.value = val;
-    }
-}
-customElements.define('hexo-editor', Editor);
-//#endregion Editor Component
+$(document).ready(function() {
+    const editor = document.getElementById('editor');
+    const easyMDE = new EasyMDE({ 
+        element: editor,
+        mode: "markdown",
+        sideBySideFullscreen: false,
+    });
+});
