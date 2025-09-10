2b44119cea60cc8c1c5d670ff988022d68a1b412
s16777216 <l1597532@gmail.com>
2025-09-11 01:41:32 +0800

    新增編輯器功能，整合標題編輯區和保存功能，並優化樣式以提升使用者體驗

--- themes/cactus/source/js/editor.js
+++ themes/cactus/source/js/editor.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
+    const postPath = window.location.pathname.replace(/^\/editor\//, '');
+
     const editor = document.getElementById('editor');
     const easyMDE = new EasyMDE({ 
         element: editor,
         mode: "markdown",
         sideBySideFullscreen: false,
+    });
+
+    const saveButton = $('#save-button');
+
+    async function onSaveButtonClick(){
+        const content = easyMDE.value();
+        await AjaxUtil.put(`/api/posts/${postPath}`, content)
+    }
+
+    async function fetchPostContent(){
+        const content = await AjaxUtil.get(`/api/posts/${postPath}`)
+        easyMDE.value(content);
+    }
+
+    saveButton.on('click', onSaveButtonClick);
+    fetchPostContent();
+
+    const headerEditor = document.querySelector('#header-editor > textarea');
+    const headerEasyMDE = new EasyMDE({ 
+        element: headerEditor,
+        toolbar: false,
+        status: false,
+        minHeight: "100px",
     });
 });
